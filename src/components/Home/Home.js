46853b323ea80801b69f587d6eae84d9986dd2df
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-24 03:26:55 +0600

    update button add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -37,6 +38,7 @@ const Home = () => {
                         key={user._id}
                     >
                         Name: {user.name} :: Email: {user.email}
+                        <Link to={`/update/${user._id}`}><button>Update</button></Link>
                         <button onClick={ () => handleUserDelete(user._id)}>X</button>
                     </li>)
                 }
---
+++ src/components/UpdateUser/UpdateUser.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const UpdateUser = () => {
+    const {id} = useParams();
+    const [user, setUser] = useState([]);
+
+    useEffect( () => {
+        const url = `http://localhost:5000/user/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setUser(data))
+    }, [])
+    return (
+        <div>
+            <h1>Updating User: {user.name}</h1>
+        </div>
+    );
+};
+
+export default UpdateUser;
